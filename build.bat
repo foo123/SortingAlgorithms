2db75027c9c58fe74aecbb51e13a3d22cc1f1180
Nikos M <nikosms0@gmail.com>
2015-01-12 02:09:09 +0200

    update buildtools

--- build.bat
+++ build.bat
@@ -3,30 +3,15 @@
 REM ###################################################
 REM #
 REM #   The buildtools repository is at:
-REM #   https://github.com/foo123/scripts/buildtools
+REM #   https://github.com/foo123/Beeld
 REM #
 REM ###################################################
 
 REM to use the python build tool do:
-REM python ..\scripts\buildtools\build.py --deps ".\dependencies"
-REM python ..\scripts\buildtools\build.py --deps ".\dependencies.ini" --compiler yui
-REM python ..\scripts\buildtools\build.py --deps ".\dependencies.yml"
-REM python ..\scripts\buildtools\build.py --deps ".\dependencies.json"
-REM python ..\scripts\buildtools\build.py --deps ".\dependencies" --compiler yui
-REM python ..\scripts\buildtools\build.py
+REM python ..\Beeld\Beeld.py --config ".\config.custom"
 
 REM to use the php build tool do:
-REM php -f ..\scripts\buildtools\build.php -- --deps=".\dependencies"
-REM php -f ..\scripts\buildtools\build.php -- --deps=".\dependencies.ini" --compiler closure
-REM php -f ..\scripts\buildtools\build.php -- --deps=".\dependencies.yml"
-REM php -f ..\scripts\buildtools\build.php -- --deps=".\dependencies.json"
-REM php -f ..\scripts\buildtools\build.php -- --deps=".\dependencies" --compiler=closure
-REM php -f ..\scripts\buildtools\build.php -- --help --deps=".\dependencies" --compiler=closure
+REM php -f ..\Beeld\Beeld.php -- --config=".\config.custom"
 
 REM to use the node build tool do:
-node ..\scripts\buildtools\build.js --deps ".\dependencies"
-REM node ..\scripts\buildtools\build.js --deps ".\dependencies.ini"
-REM node ..\scripts\buildtools\build.js --deps ".\dependencies.yml"
-REM node ..\scripts\buildtools\build.js --deps ".\dependencies.json"
-REM node ..\scripts\buildtools\build.js --deps ".\dependencies" --compiler yui
-REM node ..\scripts\buildtools\build.js
+node ..\Beeld\Beeld.js --config ".\config.custom"
